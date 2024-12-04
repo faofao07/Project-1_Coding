7b04d1d5bd0747f0bf093a2e3e2bfe664def3f71
faofao07 <sardtarapol@gmail.com>
2024-12-04 16:06:33 +0700

    Add files via upload

---
+++ 1form_07Faofao.py
@@ -0,0 +1,10 @@
+print("Welcome to Pro. ครูพี่เฟา Phase 2")
+
+print("\n\n สวัสดีครับ เชิญกรอกข้อมูลการติวครับ")
+name = str (input("ชื่อเล่น"))
+subject = str (input ("วิชาที่ต้องการติว"))
+time = str (input ("ช่วงเวลาที่ต้องการติว"))
+
+print("\n\n ขอต้อนรับและสวัสดีคุณ", name)
+print("คุณต้องการติววิชา", subject)
+print("คุณต้องการติวในช่วงเวลา", time)
